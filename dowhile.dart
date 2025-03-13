34ba380757b5422e4925c9405aa231f532477729
Muhammad Arrahman <arrahmanmuhammad16@gmail.com>
2025-03-13 14:05:48 +0800

    first commit

---
+++ dowhile.dart
@@ -0,0 +1,9 @@
+void main(){
+  // di lakukan pengecekan terlebih dahulu baru di cetak
+  var counter = 10;
+  do {
+    print('Perulangan ke $counter');
+    counter++;
+  }while (counter <= 100); 
+
+}
---
+++ forLoop.dart
@@ -0,0 +1,17 @@
+void main(){
+        //init statement    // post statement
+    for(int i = 0; i <=10 ; i++ ){
+      print('For loop');
+    };
+     
+     var counter = 1;
+
+     for (;counter <= 10;){ 
+        print('Perulangan ke $counter');
+        counter++;
+     }
+  for (var cou = 1; cou<= 10;){ 
+    print('Perulangan ke $cou');
+    cou++;
+  }
+}
